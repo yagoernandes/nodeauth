01092241379a73a2993f9a5cfe580cef9351045c
YagoErnandes <yago700@gmail.com>
2019-05-11 17:06:54 -0300

    testes de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const routes = require('express').Router()
-// const { User } = require('./app/models')
 
-// User.create({ name: 'Yago', email: 'teste@gmail.com', password_hash: '123123123123123' })
+const authMiddleware = require('./app/middleware/auth')
 
 const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.get('/dashboard', (req, res) => {
+   return res.status(200).send()
+})
 
 module.exports = routes
