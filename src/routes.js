695d2745ce7303e560a547c9b9384bef579a2e85
YagoErnandes <yago700@gmail.com>
2019-05-11 13:51:13 -0300

    esqueleto pronto

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router()
 const { User } = require('./app/models')
 
-User.create({ name: 'Yago', email: 'yago700@gmail.com', password_hash: '123123123123123' })
+User.create({ name: 'Yago', email: 'teste@gmail.com', password_hash: '123123123123123' })
 
 module.exports = routes
